8a213a2894866dbd5d5b2d5a7ac3a8044be44ae5
Denis Kiryanov <denis.kiryanov@arcadia.spb.ru>
2021-10-24 16:14:20 +0300

    Console interface added.

--- Pic12/Pic12.cpp
+++ Pic12/Pic12.cpp
@@ -8,24 +8,52 @@
 #include "ChangeAnalizer.h"
 #include "Interpretator.h"
 #include "FileService.h"
+#include <iostream>
 
 using namespace std;
 
+const int DEBUG_MODE = 1;
+
 int main(int argc, char **argv)
 {
+	cout << "PIC12 Emulator" << endl;
+	cout << "1: DEBUG mode" << endl;
+	std:cout << "2: NON-DEBUG mode" << endl;
+
+	int mode;
+	cin >> mode;
+
+	mode == DEBUG_MODE ? 
+		cout << "you have chosen the DEBUG mode..." << endl : 
+	cout << "you have cosen NON-DEBUG mode" << endl;
+
 	Processor processor;
 	FileService fileService;
 
-	fileService.LoadFromHexFile("loop.hex", processor);
+	string fileName;
+	cout << "type the path to your .hex file: ";
+	cin >> fileName;
+	cout << endl;
+
+	fileService.LoadFromHexFile(fileName.c_str(), processor);
+
+	cout << "data has been loaded successfully..." << endl;
 
 	Interpretator interpretator;
 	ChangeAnalizer analyzer;
+
+	cout << "Starting PIC12 emulation..." << endl << endl;
 
 	while (true)
 	{
 		Processor previous = processor;
 		interpretator.Interpret(processor);
-		getchar();
+
+		if (mode == DEBUG_MODE)
+		{
+			getchar();
+		}
+		
 		analyzer.AnalyzeChanges(previous, processor);
 		fileService.SaveProgramMemory(processor);
 	}
